fc671a66999f49828fb46efa4418b44c116559f4
Unknown <analauraa.planet@gmail.com>
2019-05-26 13:11:47 -0500

    Carpetas

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'localhost' );      // HOST
+  define( 'DB_USER', 'root' );          // USUARIO
+  define( 'DB_PASS', '' );             // PASSWORD
+  define( 'DB_NAME', 'bitclim' );     // NOMBRE DE LA BASE DE DATOS
+
+?>
